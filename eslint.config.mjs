f506eafb27e848daa34a26be5c63c6f9e8411c55
Thomas Scheiber <administrator@europetalks.eu>
2026-02-15 04:07:49 +0100

    chore: get-ready (deps update, lint, build)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,7 @@
-import nextConfig from "eslint-config-next";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-export default nextConfig;
-
+export default defineConfig([
+  ...nextVitals,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts", "node_modules/**"]),
+]);
